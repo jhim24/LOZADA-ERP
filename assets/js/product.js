ed1462ce0f96aa08853ce32bd9dfbfb00f0aad0f
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-06-30 11:22:38 +0300

    Create product.js

---
+++ assets/js/product.js
@@ -0,0 +1,44 @@
+// ===============================================
+// LOZADA ERP
+// PRODUCT MANAGEMENT
+// ===============================================
+
+// ---------- COMPONENT LOADER ----------
+
+async function loadComponent(id,file){
+
+    try{
+
+        const response=await fetch(file);
+
+        if(!response.ok){
+
+            throw new Error(file);
+
+        }
+
+        const html=await response.text();
+
+        document.getElementById(id).innerHTML=html;
+
+    }catch(err){
+
+        console.error(err);
+
+    }
+
+}
+
+// ---------- INITIALIZE ----------
+
+document.addEventListener("DOMContentLoaded",async()=>{
+
+    await loadComponent("sidebar","../components/sidebar.html");
+
+    await loadComponent("navbar","../components/navbar.html");
+
+    await loadComponent("product-form","../components/product-form.html");
+
+    await loadComponent("product-table","../components/product-table.html");
+
+});

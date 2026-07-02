ac51b68b85507f839a969b760f673e961943d785
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-03 00:28:19 +0300

    Update sales.js

--- assets/js/sales.js
+++ assets/js/sales.js
@@ -1,1 +1,32 @@
+// ===============================================
+// LOZADA ERP
+// SALES REPORT
+// ===============================================
 
+// ---------- COMPONENT LOADER ----------
+
+async function loadComponent(id,file){
+
+    const response = await fetch(file);
+
+    const html = await response.text();
+
+    document.getElementById(id).innerHTML = html;
+
+}
+
+// ---------- INITIALIZE ----------
+
+document.addEventListener("DOMContentLoaded", async()=>{
+
+    await loadComponent("sidebar","../components/sidebar.html");
+
+    await loadComponent("navbar","../components/navbar.html");
+
+    await loadComponent("sales-header","../components/sales-header.html");
+
+    await loadComponent("sales-content","../components/sales-content.html");
+
+    loadSales();
+
+});

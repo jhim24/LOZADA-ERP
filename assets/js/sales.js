18af6b9b59b9fdab2729fb0860f4409caa69cda7
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-03 00:30:25 +0300

    Update sales.js

--- assets/js/sales.js
+++ assets/js/sales.js
@@ -30,3 +30,103 @@ document.addEventListener("DOMContentLoaded", async()=>{
     loadSales();
 
 });
+// ===============================================
+// LOAD SALES
+// ===============================================
+
+function loadSales(){
+
+    const tbody = document.getElementById("salesTableBody");
+
+    if(!tbody) return;
+
+    const orders = JSON.parse(
+
+        localStorage.getItem("orders")
+
+    ) || [];
+
+    if(orders.length===0){
+
+        tbody.innerHTML=`
+
+        <tr>
+
+            <td colspan="7" class="text-center text-muted">
+
+                No Sales Record
+
+            </td>
+
+        </tr>
+
+        `;
+
+        return;
+
+    }
+
+    let html="";
+
+    let totalSales=0;
+
+    orders.forEach(order=>{
+
+        totalSales += Number(order.total);
+
+        html += `
+
+        <tr>
+
+            <td>${order.receiptNo}</td>
+
+            <td>${order.date}</td>
+
+            <td>${order.table}</td>
+
+            <td>${order.customer}</td>
+
+            <td>${order.payment}</td>
+
+            <td>
+
+                ₱${Number(order.total).toFixed(2)}
+
+            </td>
+
+            <td>
+
+                <button
+                class="btn btn-sm btn-primary">
+
+                    View
+
+                </button>
+
+            </td>
+
+        </tr>
+
+        `;
+
+    });
+
+    tbody.innerHTML = html;
+
+    document.getElementById("todaySales").innerHTML =
+
+    "₱" + totalSales.toFixed(2);
+
+    document.getElementById("todayOrders").innerHTML =
+
+    orders.length;
+
+    document.getElementById("todayCustomers").innerHTML =
+
+    orders.length;
+
+    document.getElementById("averageSale").innerHTML =
+
+    "₱" + (totalSales/orders.length).toFixed(2);
+
+}

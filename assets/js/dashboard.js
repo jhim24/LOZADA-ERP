1c3a2fd6381b2f2e3db44c9ce79a92e7818d7b2f
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-08 23:11:55 +0300

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -47,8 +47,7 @@ document.addEventListener("DOMContentLoaded", async function(){
     await loadComponent("sidebar","../components/sidebar.html");
 
     await loadComponent("navbar","../components/navbar.html");
-    initializeSidebarToggle();
-  
+   
     await loadComponent("dashboard-cards","../components/dashboard-cards.html");
 
     await loadComponent("dashboard-charts","../components/dashboard-charts.html");

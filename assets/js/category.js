3ea0a45d24c1f8fdfa4247546726fe2be4dfaa8c
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-09 19:03:27 +0300

    Update category.js

--- assets/js/category.js
+++ assets/js/category.js
@@ -3,6 +3,34 @@
 // CATEGORY MANAGEMENT
 // ===============================================
 
+// FIREBASE
+
+const firebaseConfig = {
+
+apiKey:"AIzaSyAV9T5w_1azmPHIJcZpraXP06TItj7HEuA",
+
+authDomain:"papprito-orders.firebaseapp.com",
+
+databaseURL:"https://papprito-orders-default-rtdb.firebaseio.com",
+
+projectId:"papprito-orders",
+
+storageBucket:"papprito-orders.firebasestorage.app",
+
+messagingSenderId:"831941801424",
+
+appId:"1:831941801424:web:40a99cdfb312dac2d275d5"
+
+};
+
+if(!firebase.apps.length){
+
+firebase.initializeApp(firebaseConfig);
+
+}
+
+const db=firebase.database();
+
 // ---------- COMPONENT LOADER ----------
 
 async function loadComponent(id, file){

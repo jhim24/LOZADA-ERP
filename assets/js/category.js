ca3289c87ec370380e8b053d5ed2f017880564fe
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-09 21:49:54 +0300

    Update category.js

--- assets/js/category.js
+++ assets/js/category.js
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
 
 let categories = [];
 
-let editIndex = -1;
+let editIndex = "";
 
 // ===============================================
 // LOAD CATEGORY TABLE

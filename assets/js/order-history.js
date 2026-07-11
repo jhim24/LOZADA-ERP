25aec2be909160bcc46e6d0abe51b28efbd6aa28
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-11 23:43:20 +0300

    Create order-history.js

---
+++ assets/js/order-history.js
@@ -0,0 +1,83 @@
+// ===============================================
+// LOZADA ERP
+// ORDER HISTORY
+// ===============================================
+
+// ===============================================
+// FIREBASE
+// ===============================================
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAV9T5w_1azmPHIJcZpraXP06TItj7HEuA",
+    authDomain: "papprito-orders.firebaseapp.com",
+    databaseURL: "https://papprito-orders-default-rtdb.firebaseio.com",
+    projectId: "papprito-orders",
+    storageBucket: "papprito-orders.firebasestorage.app",
+    messagingSenderId: "831941801424",
+    appId: "1:831941801424:web:40a99cdfb312dac2d275d5"
+};
+
+if(!firebase.apps.length){
+
+    firebase.initializeApp(firebaseConfig);
+
+}
+
+const db = firebase.database();
+
+// ===============================================
+// COMPONENT LOADER
+// ===============================================
+
+async function loadComponent(id,file){
+
+    try{
+
+        const response = await fetch(file);
+
+        if(!response.ok){
+
+            throw new Error(file);
+
+        }
+
+        document.getElementById(id).innerHTML =
+            await response.text();
+
+    }catch(error){
+
+        console.error(error);
+
+    }
+
+}
+
+// ===============================================
+// INITIALIZE
+// ===============================================
+
+document.addEventListener("DOMContentLoaded",async()=>{
+
+    await loadComponent(
+        "sidebar",
+        "../components/sidebar.html"
+    );
+
+    await loadComponent(
+        "navbar",
+        "../components/navbar.html"
+    );
+
+    await loadComponent(
+        "history-header",
+        "../components/history-header.html"
+    );
+
+    await loadComponent(
+        "history-table",
+        "../components/history-table.html"
+    );
+
+    loadHistory();
+
+});
